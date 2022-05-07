eb06ad962f0109a9f40780c31a7ba7b833dcc310
kange <kangerts9@gmail.com>
2022-05-07 18:16:09 +0300

    task8

--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.task3
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
 import com.example.task3.App.Companion.navigatorHolder
 import com.example.task3.App.Companion.router
 import com.example.task3.navigation.CustomNavigator
 import com.example.task3.word.WordScreen
 
 class MainActivity : AppCompatActivity() {
+    private val COUNTDOWN_INTERVAL: Long = 1000
+    private val COUNTDOWN_DURATION: Long = 2000
+    private val SLIDE_LEFT_DURATION: Long = 1000L
     private val navigator = CustomNavigator(activity = this, R.id.content)
 
     override fun onResumeFragments() {
@@ -15,8 +26,10 @@ class MainActivity : AppCompatActivity() {
         navigatorHolder.setNavigator(navigator)
     }
 
+    @RequiresApi(31)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setDefaultSplashScreen()
         if (savedInstanceState == null) {
             router.navigateTo(WordScreen)
         }
@@ -25,5 +38,50 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         navigatorHolder.removeNavigator()
         super.onPause()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setDefaultSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            setSplashScreenHideAnimation()
+        }
+        setSplashScreenDuration()
+    }
+    @RequiresApi(31)
+    private fun setSplashScreenHideAnimation() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideLeft = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_X,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideLeft.interpolator = AnticipateInterpolator()
+            slideLeft.duration = SLIDE_LEFT_DURATION
+            slideLeft.doOnEnd { splashScreenView.remove() }
+            slideLeft.start()
+        }
+    }
+    private fun setSplashScreenDuration() {
+        var isHideSplashScreen = false
+        object : CountDownTimer(COUNTDOWN_DURATION, COUNTDOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
     }
 }
